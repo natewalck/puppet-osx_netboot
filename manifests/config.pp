0bdfb825bfd5f085f07c13adb76f292cec6bd740
natewalck <nate@osxadm.in>
2012-12-10 10:55:00 -0500

    Changed a file template to execs since it required few keys

--- manifests/config.pp
+++ manifests/config.pp
@@ -12,12 +12,24 @@ class applenetboot::config ( $interface = $applenetboot::params::interface)
     owner   => '0',
   }
 
-  file { '/private/etc/bootpd.plist':
-    ensure  => present,
-    group   => '0',
-    mode    => '0644',
-    owner   => '0',
-    content => template('applenetboot/bootpd.plist.erb')
+  exec { 'bootp_enabled':
+    path    => '/bin:/usr/bin',
+    command => 'defaults write /etc/bootpd bootp_enabled -bool true',
+    unless  => 'defaults read /etc/bootpd bootp_enabled | grep -qx 1',
+  }
+
+  exec { 'dhcp_enabled_false':
+    path    => '/bin:/usr/bin',
+    command => 'defaults write /etc/bootpd dhcp_enabled -bool false',
+    unless  => 'defaults read /etc/bootpd dchp_enabled | grep -qx 0',
+  }
+
+  exec { 'netboot_enabled':
+    path    => '/bin:/usr/bin',
+    command => "defaults write /etc/bootpd netboot_enabled\
+                                          -array-add ${interface}",
+    unless  => "defaults read /etc/bootpd netboot_enabled\
+                                          | grep -q ${interface}",
   }
 
   file { '/private/etc/exports':
@@ -32,7 +44,9 @@ class applenetboot::config ( $interface = $applenetboot::params::interface)
   service { 'com.apple.bootpd':
     ensure  => running,
     enable  => true,
-    require => [ File['/private/etc/bootpd.plist'],
+    require => [ Exec['bootp_enabled'],
+                      Exec['dhcp_enabled_false'],
+                      Exec['netboot_enabled'],
                       Service['com.apple.tftpd']
                     ]
   }
